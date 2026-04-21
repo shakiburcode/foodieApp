377a71d5a0d948216f216a8242b2608bb8693ff8
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-04-21 20:13:26 +0600

    Add a blank line for better readability in tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:food_recipe_app/main.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
